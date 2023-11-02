7e0057d61bedc1951c22ae898185591541ee22b8
Sanka Peeris <sankapeeris360@gmail.com>
2023-11-02 10:53:11 +0530

    add git config

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
 # Install the fonts
-stow -t ~/.local/share/fonts -S fonts
-fc-cache -fv
+# stow -t ~/.local/share/fonts -S fonts
+# fc-cache -fv
+
+ln -sf $PWD/git $HOME/.config/git
+ln -sf $PWD/.vimrc $HOME/.vimrc
+ln -sf $PWD/tmux $HOME/.config/tmux
+
